850961029610d86ab6e87b4c2d14de300abc5d66
Yevgeniy Davydov <yevgeniydavydov@Yevgeniys-MBP.fritz.box>
2024-01-23 22:52:59 +0100

    files added

---
+++ JS/script.js
@@ -0,0 +1,89 @@
+"use strict"
+
+// 1 Вивод парних чисел (for)
+
+// let value = +prompt("Введить числове значення, бiлше нiж 2");
+
+
+// if (isNaN(value) === true) {
+//     alert("Значення не є числом");
+
+// } 
+// else if (value < 2) {
+//     alert("Значення менше нiж 2");
+
+// }
+// else {
+//    for (let i = 2; i < value; i +=2) {
+//  console.log(i);
+// }
+// };
+
+
+
+// // Вивод парних чисел (while)
+
+// let value = +prompt("Введить числове значення, бiлше нiж 2");
+
+
+// if (isNaN(value) === true) {
+//     alert("Значення не є числом");
+
+// } 
+// else if (value < 2) {
+//     alert("Значення менше нiж 3");
+
+// }
+// else {
+//    let i = 2;
+//    while (i < value) {
+//     console.log(i);
+//     i +=2;
+//    }
+// };
+
+// // 2 Задача FizzBuzz
+
+
+// for (let i = 1; i <=100; i++) {
+
+//     if (i % 3 === 0 && i % 5 ===0){
+//         console.log("FizzBuzz");
+//     }
+//     else if (i % 3 === 0){
+//         console.log("Fizz");
+//     }
+//     else if (i % 5 === 0){
+//         console.log("Buzz");
+//     } else {
+//         console.log(i)
+//     }
+
+// };
+
+
+// 3 Додаткова задача
+
+
+// let string = '42559125';
+
+// let result = "";
+
+
+// for (let i = 0; i < string.length; i ++) {
+
+//     if (string[i] < 5){
+//         result += "0" 
+//     } else{
+//         result += "1"
+//     }
+// };
+
+// console.log(result);
+
+
+
+
+
+
+
